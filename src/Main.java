57812d0d62231c950b48e80c33e480bddda5b534
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2024-02-09 21:18:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        counter(3);
+        System.out.println();
+        System.out.println(fac(4));
+    }
+
+    private static void counter(int n) {
+        if(n==0)
+            return;
+
+        System.out.println(n);
+        counter(n-1);
+    }
+
+    private static int fac(int n) {
+        if(n==1)
+            return 1;
+
+        return n * fac(n-1);
+    }
+}
